f4ba0f9f5fc0f7b60370c18c68ee23450083af28
awinel <benjamin.huerta.awi@gmail.com>
2022-09-23 14:54:36 -0300

    class Card finished update

--- card.py
+++ card.py
@@ -1,3 +1,5 @@
+import random
+
 class Card:
     """A pice of paper or plastic that have a number on it.
 
@@ -5,4 +7,15 @@ class Card:
     
     Attributes:
         card_num (int): The number that is on the card"""
-        
+    
+    def __init__(self):
+        """Construct a new instance of Card.
+        Arg:
+            self (Card): An instance of card.
+        """
+        self.card_num = 0
+
+    def suffle(self):
+        """Generates a random card from 1 to 13 to the dealer to show.
+        """
+        self.card_num = random.randint(1, 13)
