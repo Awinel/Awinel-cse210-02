a5568b5a4573cabab76b21200bb0569787df8763
awinel <benjamin.huerta.awi@gmail.com>
2022-09-23 14:31:18 -0300

    first update

---
+++ card.py
@@ -0,0 +1,8 @@
+class Card:
+    """A pice of paper or plastic that have a number on it.
+
+    The responsability of a Card is to keep a number between 1-13.
+    
+    Attributes:
+        card_num (int): The number that is on the card"""
+        
